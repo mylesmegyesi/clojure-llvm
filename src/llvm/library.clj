4d2ac4c2bf37b97bc406adeae0d369360f0d5a8c
Myles Megyesi <mylesmegyesi@me.com>
2013-12-06 13:54:55 -0600

    something is working

--- src/llvm/library.clj
+++ src/llvm/library.clj
@@ -1,12 +1,9 @@
 (ns llvm.library
-  (:require [clojure.java.shell :refer [sh]]
-            [clojure.string     :refer [trim]])
   (:import jnr.ffi.LibraryLoader llvm.LLVMWrapper))
 
 (def llvm-library
-  (let [libdir (-> (sh "llvm-config" "--libdir") :out trim)]
-    (delay
-      (-> llvm.LLVMWrapper
-        LibraryLoader/create
-        (.search libdir)
-        (.load "LLVM-3.2")))))
+  (delay
+    (-> llvm.LLVMWrapper
+      LibraryLoader/create
+      (.search "llvm")
+      (.load "LLVM-3.2"))))
